2ee6f0d88ae451c349d468d1e2ba5cab8dc03fd4
Uğur <149895205+ugurbenli48@users.noreply.github.com>
2025-04-20 18:13:53 +0300

    Update veritabani_islemler.php

--- veritabani_islemler.php
+++ veritabani_islemler.php
@@ -24,4 +24,33 @@ if (isset($_POST['xd']) && $_POST['xd'] == "kayitol") {
         }
     }
 }
+if($_POST['xd'] =='sorular') {
+	
+	$sorgu="select * from sorular";
+    $sorguSonuc=$baglanti->query($sorgu);
+
+if($sorguSonuc->num_rows>0) {
+	
+	$butunSatirlar=array();
+while($row=$sorguSonuc->fetch_object()) {
+
+      array_push($butunSatirlar,array(
+	  
+	  "soru" =>$row ->soru,
+	  "a_Cevab"=> $row ->a_Cevabi,
+      "b_Cevab"=> $row ->b_Cevabi,
+      "c_Cevab"=> $row ->c_Cevabi,
+      "d_Cevab"=> $row ->d_Cevabi,
+      "dogruCevab"=> $row ->dogruCevap,
+     )
+     );	 
+}
+echo json_encode(array("butunSorular"=>$butunSatirlar));
+}
+else { //Hata 
+}
+}
+$baglanti->close();
+
+
 ?>
